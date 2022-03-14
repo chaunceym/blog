1b323ae8726bad55e6ed3b0e181022ff4670de89
孟祥宇 <18322276773@163.com>
2022-03-14 18:01:03 +0800

    feat: 存入cookie

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,8 @@ module MorneyRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.session_store :cookie_store, key: '_morney_session_id'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
