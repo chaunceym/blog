e9c48586e6b64cb42e8923a81e0edfbb5de62e6b
孟祥宇 <18322276773@163.com>
2022-03-14 20:57:11 +0800

    feat: 获取当前用户信息

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::API
+  def current_user
+    @current_user ||= User.find_by_id session[:current_user_id]
+  end
   def render_resource(source)
+    return head 404 if source.nil?
     if source.errors.empty?
       render json: { resource: source }, status: 200
     else
