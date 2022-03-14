e48a1ab0832582abca50ab0b52c976c0a5c080fd
孟祥宇 <18322276773@163.com>
2022-03-14 22:09:22 +0800

    feat: 删除session

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/hello', to: 'first#hello'
   get '/me', to: 'users#me'
+  delete '/sessions', to: 'sessions#destroy'
 
   resources :users
-  resources :sessions, only: [:create,:destroy]
+  resources :sessions, only: [:create]
 end
